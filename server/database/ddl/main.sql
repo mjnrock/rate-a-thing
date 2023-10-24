8aa1d87604ab83e2e784685fe45466e10f413729
LesPantsFancy <mjnrock@umich.edu>
2023-10-24 09:08:54 -0400

    wip

--- server/database/ddl/main.sql
+++ server/database/ddl/main.sql
@@ -4,11 +4,8 @@
 -- CREATE SCHEMA Core
 -- GO
 
-IF OBJECT_ID('Core.Entry') IS NOT NULL DROP TABLE Core.[Entry];
 IF OBJECT_ID('Core.Instance') IS NOT NULL DROP TABLE Core.[Instance];
-IF OBJECT_ID('Core.Structure') IS NOT NULL DROP TABLE Core.[Structure];
 IF OBJECT_ID('Core.Template') IS NOT NULL DROP TABLE Core.[Template];
-IF OBJECT_ID('Core.Element') IS NOT NULL DROP TABLE Core.[Element];
 IF OBJECT_ID('Core.Namespace') IS NOT NULL DROP TABLE Core.[Namespace];
 GO
 
@@ -26,39 +23,15 @@ CREATE TABLE Core.[Namespace] (
 INSERT INTO Core.[Namespace] ([Name])
 VALUES ('RateAThing');  -- A default Namespace for the system
 
-/* A repository for storing the raw schema of a given Element */
-CREATE TABLE Core.Element (
-    ElementID INT IDENTITY(1,1) PRIMARY KEY,
-    NamespaceID INT NOT NULL REFERENCES Core.[Namespace](NamespaceID) DEFAULT 1,  -- A default storage location for Elements
-    [Schema] NVARCHAR(MAX) NOT NULL,
-
-    [Type] AS CAST(JSON_VALUE([Schema], '$.$type') AS VARCHAR(255)) PERSISTED,
-    [SubType] AS CAST(JSON_VALUE([Schema], '$.$subtype') AS VARCHAR(255)) PERSISTED,
-    Tags AS CAST(JSON_QUERY([Schema], '$.$tags') AS NVARCHAR(MAX)) PERSISTED,
-
-    UUID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
-    CreatedDateTimeUTC DATETIME2(0) NOT NULL DEFAULT SYSUTCDATETIME(),
-    ModifiedDateTimeUTC DATETIME2(0) NOT NULL DEFAULT SYSUTCDATETIME(),
-    DeactivatedDateTimeUTC DATETIME2(0) NULL
-);
-
 /* An architectural grouping for nesting Elements */
 CREATE TABLE Core.Template (
     TemplateID INT IDENTITY(1,1) PRIMARY KEY,
-    NamespaceID INT NOT NULL REFERENCES Core.[Namespace](NamespaceID) DEFAULT 1,  -- Allows Templates to be in Namespaces that are different from their Elements
+    NamespaceID INT NOT NULL REFERENCES Core.[Namespace](NamespaceID) DEFAULT 1,
+    
     [Name] NVARCHAR(255) NOT NULL,
-
-    UUID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
-    CreatedDateTimeUTC DATETIME2(0) NOT NULL DEFAULT SYSUTCDATETIME(),
-    ModifiedDateTimeUTC DATETIME2(0) NOT NULL DEFAULT SYSUTCDATETIME(),
-    DeactivatedDateTimeUTC DATETIME2(0) NULL
-);
-/* The "row level" of a Template, which is a single Element and where it should be reconsigned in the Template */
-CREATE TABLE Core.Structure (
-    StructureID INT IDENTITY(1,1) PRIMARY KEY,
-    TemplateID INT NOT NULL REFERENCES Core.[Template](TemplateID),
-    ElementID INT NOT NULL REFERENCES Core.Element(ElementID),
-    [Path] NVARCHAR(MAX) NOT NULL,
+    [Schema] NVARCHAR(MAX) NOT NULL,    -- This should be the raw JSON Schema for the Template (i.e. the entire template should be reconstitutable from this field)
+    Tags AS CAST(JSON_QUERY([Schema], '$.$tags') AS NVARCHAR(MAX)) PERSISTED,   -- Extract the Tags from the Schema for better querying
+    [Version] INT NOT NULL DEFAULT 1,   -- A simple versioning to better support the evolution of Templates vis-a-vis their Instances
 
     UUID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
     CreatedDateTimeUTC DATETIME2(0) NOT NULL DEFAULT SYSUTCDATETIME(),
@@ -70,22 +43,32 @@ CREATE TABLE Core.Structure (
 CREATE TABLE Core.Instance (
     InstanceID INT IDENTITY(1,1) PRIMARY KEY,
     TemplateID INT NOT NULL REFERENCES Core.[Template](TemplateID),
-    Context NVARCHAR(MAX) NOT NULL,
+    [Version] INT NOT NULL DEFAULT 1, -- The version of the Template that this Instance is based on
+    [Data] NVARCHAR(MAX) NOT NULL,  -- This should be a flattened UUID:Value mapping of the Template's elements
 
     UUID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
     CreatedDateTimeUTC DATETIME2(0) NOT NULL DEFAULT SYSUTCDATETIME(),
     ModifiedDateTimeUTC DATETIME2(0) NOT NULL DEFAULT SYSUTCDATETIME(),
     DeactivatedDateTimeUTC DATETIME2(0) NULL
 );
-/* The "row level" data of an instantiation of a Template */
-CREATE TABLE Core.[Entry] (
-    EntryID INT IDENTITY(1,1) PRIMARY KEY,
-    InstanceID INT NOT NULL REFERENCES Core.Instance(InstanceID),
-    ElementID INT NOT NULL REFERENCES Core.Element(ElementID),
-    [Value] NVARCHAR(MAX) NOT NULL,
+GO
 
-    UUID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
-    CreatedDateTimeUTC DATETIME2(0) NOT NULL DEFAULT SYSUTCDATETIME(),
-    ModifiedDateTimeUTC DATETIME2(0) NOT NULL DEFAULT SYSUTCDATETIME(),
-    DeactivatedDateTimeUTC DATETIME2(0) NULL
-);
+
+-- Create a change trigger that increments Version by 1 any time the Schema is updated
+CREATE TRIGGER Core.Template_UpdateVersion
+ON Core.Template
+AFTER UPDATE
+AS
+BEGIN
+    IF UPDATE([Schema])
+    BEGIN
+        UPDATE t
+        SET
+            t.[Version] = t.[Version] + 1,
+            t.ModifiedDateTimeUTC = SYSUTCDATETIME()
+        FROM
+            Core.Template t
+            INNER JOIN inserted i
+                ON i.TemplateID = t.TemplateID
+    END
+END
