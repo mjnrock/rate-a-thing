48179de5c5e672c4fe81c4098a32340a248dfc4e
LesPantsFancy <mjnrock@umich.edu>
2023-10-28 10:53:30 -0400

    rf

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 
 import Default from "./routes/Default";
-import Ratings from "./routes/Ratings";
-import Review from "./routes/Review";
-import Reviews from "./routes/Reviews";
+import Review_0_1_0 from "./routes/Review-0.1.0";
+import Review_0_2_0 from "./routes/Review-0.2.0";
+import Review_0_3_0 from "./routes/Review-0.3.0";
 
 export function App() {
 	return (
 		<Routes>
 			<Route path="/" element={ <Default /> } />
-			<Route path="/ratings" element={ <Review /> } />
-			<Route path="/reviews" element={ <Reviews /> } />
+			<Route path="/review-0.1.0" element={ <Review_0_1_0 /> } />
+			<Route path="/review-0.2.0" element={ <Review_0_2_0 /> } />
+			<Route path="/review-0.3.0" element={ <Review_0_3_0 /> } />
 		</Routes>
 	);
 };
---
+++ src/routes/Review-0.3.0.jsx
@@ -0,0 +1,15 @@
+import Chord from "@lespantsfancy/chord";
+
+const Nodes = Chord.Node.Node.CreateMany({
+});
+
+export function Reviews() {
+	// const { state: reviewsState, dispatch: reviewsDispatch } = Chord.Node.React.useNode(Nodes.reviews);
+	return (
+		<div>
+			Hi
+		</div>
+	)
+};
+
+export default Reviews;
