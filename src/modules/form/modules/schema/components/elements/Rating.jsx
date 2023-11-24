a7bdcc4c6cb76c4245af13cd6a58b892429aaed1
LesPantsFancy <mjnrock@umich.edu>
2023-11-24 13:32:44 -0500

    wip

--- src/modules/form/modules/schema/components/elements/Rating.jsx
+++ src/modules/form/modules/schema/components/elements/Rating.jsx
@@ -29,7 +29,7 @@ function Rating({ update, element }) {
 			stars.push(
 				<Comp
 					className={ `cursor-pointer hover:text-amber-300 active:text-amber-400 ` + (i <= element.state.value ? "text-neutral-500" : "text-neutral-300") }
-					onClick={ e => update("setElementValue", element.id, i) }
+					onClick={ e => update("setElementValue", element.id, Math.min(Math.max(i, min), max)) }
 					key={ i }
 					size={ "3rem" }
 				/>
@@ -44,17 +44,35 @@ function Rating({ update, element }) {
 			<div className="flex flex-row items-center justify-between w-full gap-2">
 				<div className="w-full p-2">
 					<div className="flex flex-1">Min</div>
-					<input type="number" max={ max } step={ step } value={ min } onChange={ e => update("mergeElementState", element.id, { min: +e.target.value }) } className="flex-1 w-full p-2 font-mono border border-solid rounded shadow-md select-none text-neutral-600 border-neutral-200" />
+					<input
+						type="number"
+						max={ max }
+						step={ step }
+						value={ min }
+						onChange={ e => update("mergeElementState", element.id, { min: +e.target.value, value: Math.max(+e.target.value, element.state.value) }) }
+						className="flex-1 w-full p-2 font-mono border border-solid rounded shadow-md select-none text-neutral-600 border-neutral-200"
+					/>
 				</div>
 				<div className="w-full p-2">
 					<div className="flex flex-1">Max</div>
-					<input type="number" min={ min } step={ step } value={ max } onChange={ e => update("mergeElementState", element.id, { max: +e.target.value }) } className="flex-1 w-full p-2 font-mono border border-solid rounded shadow-md select-none text-neutral-600 border-neutral-200" />
+					<input
+						type="number"
+						min={ min }
+						step={ step }
+						value={ max }
+						onChange={ e => update("mergeElementState", element.id, { max: +e.target.value, value: Math.min(+e.target.value, element.state.value) }) }
+						className="flex-1 w-full p-2 font-mono border border-solid rounded shadow-md select-none text-neutral-600 border-neutral-200"
+					/>
 				</div>
 			</div>
 			<div className="flex flex-row items-center justify-between w-full gap-2">
 				<div className="w-full p-2">
 					<div className="flex flex-1">Icon</div>
-					<select value={ stateIcon } onChange={ e => update("mergeElementState", element.id, { icon: e.target.value }) } className="flex-1 w-full p-2 font-mono border border-solid rounded shadow-md select-none text-neutral-600 border-neutral-200">
+					<select
+						value={ stateIcon }
+						onChange={ e => update("mergeElementState", element.id, { icon: e.target.value }) }
+						className="flex-1 w-full p-2 font-mono border border-solid rounded shadow-md select-none text-neutral-600 border-neutral-200"
+					>
 						<option value="star">Star</option>
 						<option value="circle">Circle</option>
 						<option value="square">Square</option>
